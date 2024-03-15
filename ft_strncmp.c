d56038e779485632f34cd4b5811210912f977e1a
marodrig <miro.ines@hotmail.com>
2024-03-15 16:42:23 +0000

    strncmp

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,21 +6,18 @@
 /*   By: marodrig <marodrig@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/23 23:31:16 by marodrig          #+#    #+#             */
-/*   Updated: 2024/02/23 23:37:05 by marodrig         ###   ########.fr       */
+/*   Updated: 2024/03/15 16:41:56 by marodrig         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int	ft_strncmp(char *s1, char *s2, unsigned int n)
-{
-	unsigned int	i;
+#include <stdio.h>
 
-	if (n == 0)
-		return (0);
+int	ft_strncmp(const char *s1, const char *s2, size_t n)
+{
+	size_t	i;
+	
 	i = 0;
 	while (s1[i] && s2[i] && i < n)
 		i++;
-	if (i < n)
-		return (s1[i] - s2[i]);
-	else
-		return (0);
+	return ((unsigned char)s1[i] - (unsigned char)s2[i]);
 }
