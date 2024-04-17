cf8c631473208393e7ef0a47983c3de6fb57b7c8
ma-rodrig <miro.ines@hotmail.com>
2024-04-17 13:26:48 +0100

    memcpy

---
+++ ft_memcpy.c
@@ -0,0 +1,46 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memcpy.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: marodrig <marodrig@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/17 12:44:34 by marodrig          #+#    #+#             */
+/*   Updated: 2024/04/17 13:24:00 by marodrig         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_memcpy(void *dest, const void *src, size_t n)
+{
+	size_t	i;
+	unsigned char	*ori;
+	unsigned char	*dst;
+
+	i = 0;
+	ori = (unsigned char *)src;
+	dst = (unsigned char *)dest;
+	if (n > 0)
+	{
+		while (i < n)
+		{
+			dst[i] = ori[i];
+			i++;
+		}
+	}
+	return (dst);
+}
+
+int	main(void)
+{
+	char src[] = "abcdefghijklm";
+    char dest[] = "nopqrst";
+    char s1[] = "abcdefghijklm";
+    char d1[] = "nopqrst";
+    ft_memcpy(dest, src, (sizeof(char)*2));
+    memcpy(d1, s1, (sizeof(char)*2));
+
+    printf("%s\n", dest);
+    printf("%s\n", d1);
+}
