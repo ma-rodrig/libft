55112f94f5c91296fce14b17ca40ce8c2f9fc28a
marodrig <miro.ines@hotmail.com>
2024-04-22 16:54:17 +0100

    memove_final

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,51 +6,51 @@
 /*   By: marodrig <marodrig@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/17 15:33:51 by marodrig          #+#    #+#             */
-/*   Updated: 2024/04/17 18:58:55 by marodrig         ###   ########.fr       */
+/*   Updated: 2024/04/22 16:54:06 by marodrig         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 //copies first n bytes from mem area src to mem area dest, may overlap
 //bytes in src are 1st copied into temp array and then to dest
+//if dst and src equal or null, return dest without performing copy
 #include "libft.h"
 
 void	*ft_memmove(void *dest, const void *src, size_t n)
 {
-	size_t			i;
 	unsigned char	*dst;
-	unsigned char	*temp;
+	unsigned char	*sce;
 
-	i = 0;
-	temp = (unsigned char *)src;
+	sce = (unsigned char *)src;
 	dst = (unsigned char *)dest;
-	if (dst > temp)
+	if (!dst && !sce)
+		return (dst);
+	if (dst < sce)
 	{
-		while (n > 0)
+		while (n--)
 		{
-			dst[i] = temp[i];
-			--n;
+			dst[n] = sce[n];
 		}
 	}
 	else
 	{
-		while (i < n)
+		while (n--)
 		{
-			dst[i] = temp[i];
-			i++;
+			*dst++ = *sce++;
 		}
 	}
 	return (dst);
 }
-
+/*
 int	main(void)
 {
 	char src[] = "abcdefghijklm";
     char dest[] = "nopqrst";
     char s1[] = "abcdefghijklm";
     char d1[] = "nopqrst";
-    ft_memmove(dest, src, (sizeof(char)*2));
-    memmove(d1, s1, (sizeof(char)*2));
+    ft_memmove(dest, src, (sizeof(char)*5));
+    memmove(d1, s1, (sizeof(char)*5));
 
     printf("%s\n", dest);
     printf("%s\n", d1);
 }
+*/
