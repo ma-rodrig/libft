9157f02948928d8448a9ca7b55928c5d1e700293
ma-rodrig <miro.ines@hotmail.com>
2024-04-24 12:46:56 +0100

    .

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,11 +6,7 @@
 /*   By: marodrig <marodrig@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/17 15:33:51 by marodrig          #+#    #+#             */
-<<<<<<< HEAD
-/*   Updated: 2024/04/18 14:59:52 by marodrig         ###   ########.fr       */
-=======
-/*   Updated: 2024/04/22 16:54:06 by marodrig         ###   ########.fr       */
->>>>>>> refs/remotes/origin/main
+/*   Updated: 2024/04/24 12:46:46 by marodrig         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,17 +26,9 @@ void	*ft_memmove(void *dest, const void *src, size_t n)
 		return (dst);
 	if (dst < sce)
 	{
-<<<<<<< HEAD
-		while (n > 0 && (i >= 0))
-		{
-			dst[i] = temp[i];
-			--n;
-			i++;
-=======
 		while (n--)
 		{
 			dst[n] = sce[n];
->>>>>>> refs/remotes/origin/main
 		}
 	}
 	else
@@ -55,17 +43,10 @@ void	*ft_memmove(void *dest, const void *src, size_t n)
 /*
 int	main(void)
 {
-<<<<<<< HEAD
-	char dest[] = "zabcdefghijklm";
-    char src[] = "nopqrst";
-    char d1[] = "zabcdefghijklm";
-    char s1[] = "nopqrst";
-=======
 	char src[] = "abcdefghijklm";
     char dest[] = "nopqrst";
     char s1[] = "abcdefghijklm";
     char d1[] = "nopqrst";
->>>>>>> refs/remotes/origin/main
     ft_memmove(dest, src, (sizeof(char)*5));
     memmove(d1, s1, (sizeof(char)*5));
 
