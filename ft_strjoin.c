86712de23bed9685374ef2ac02b0c9527c1af5e0
marodrig <miro.ines@hotmail.com>
2024-04-25 12:04:15 +0100

    strjoin

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strjoin.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: marodrig <marodrig@student.42.fr>          +#+  +:+       +#+        */
+/*   By: marodrig <marodrig@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/24 17:40:56 by marodrig          #+#    #+#             */
-/*   Updated: 2024/04/24 17:56:49 by marodrig         ###   ########.fr       */
+/*   Updated: 2024/04/25 12:03:39 by marodrig         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,5 +16,25 @@
 
 char	*ft_strjoin(char const *s1, char const *s2)
 {
-	
+	char	*cat;
+	size_t	len;
+
+	if (s1 == NULL || s2 == NULL)
+		return (NULL);
+	len = (ft_strlen(s1) + ft_strlen(s2) + 1);
+	cat = ft_calloc(len, (sizeof(char)));
+	if (!cat)
+		return (NULL);
+	ft_strlcpy(cat, s1, len);
+	ft_strlcat(cat, s2, len);
+	return (cat);
 }
+/*
+int	main(void)
+{
+	char	s1[] = "string";
+	char	s2[] = " was joined";
+		
+	printf("%s\n", ft_strjoin(s1, s2));	
+}
+*/
