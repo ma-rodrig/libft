1658c88e6ea4cbfa969923b810f2ba5d9e8cb1ec
marodrig <miro.ines@hotmail.com>
2024-04-25 15:49:07 +0100

    trimunfinished

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: marodrig <marodrig@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/25 12:05:36 by marodrig          #+#    #+#             */
-/*   Updated: 2024/04/25 12:07:48 by marodrig         ###   ########.fr       */
+/*   Updated: 2024/04/25 15:48:57 by marodrig         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,41 @@
 //in set removed from the beggining and end of the str
 //returns the trimmed string, null if alloc fails
 
+
+//CORRIGIR NAO ACABADO
+
+
+
 #include "libft.h"
 
 char	*ft_strtrim(char const *s1, char const *set)
 {
-	
+	size_t	i;
+	size_t	j;
+	char	*trim;
+
+	i = 0;
+	j = 0;
+	trim = malloc((sizeof(char)) * (ft_strlen(set) + 1));
+	if (!trim)
+		return (NULL);
+	while (s1[i])
+	{
+		if (set[i] == s1[i])
+		{
+			trim[j] = s1[i];
+			j++;
+		}
+		i++;
+	}
+	trim[j] = '\0';
+	return (trim);
+}
+
+int	main(void)
+{
+	char	s1[] = "string trimmed here";
+	char	set[] = "trimmed";
+
+	printf("%s\n", ft_strtrim(s1, set));
 }
