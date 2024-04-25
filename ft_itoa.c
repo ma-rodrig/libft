84564d570271e7d6e2bce596a26d5e8bc95b344e
marodrig <miro.ines@hotmail.com>
2024-04-25 16:04:01 +0100

    itoa

---
+++ ft_itoa.c
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_itoa.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: marodrig <marodrig@student.42lisboa.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/25 15:49:40 by marodrig          #+#    #+#             */
+/*   Updated: 2024/04/25 15:51:13 by marodrig         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+
+//allocs and returns a str representing the int
+// received as an argument, negative nbrs must be handled
+
+#include "libft.h"
+
+char	*ft_itoa(int n)
+{
+	
+}
